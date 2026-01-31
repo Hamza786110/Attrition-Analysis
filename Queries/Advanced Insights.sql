525f79716889ce26d55d3c2c12ca8ffb26648449
Hamza Nathwala <hamza.parvezali@gmail.com>
2026-01-31 18:20:22 +0530

    Update Advanced Insights.sql

--- Queries/Advanced Insights.sql
+++ Queries/Advanced Insights.sql
@@ -1,19 +1,26 @@
--- select department,(sum(case when Attrition='Yes' then 1 else 0 end)*100.0/count(*)) as Attrition,avg(JobSatisfaction) as AVG_JOB_SATISFACTION from retentiondata
--- group by department
--- order by Attrition desc,
--- AVG_JOB_SATISFACTION ASC
 
--- select (avg(case when overtime='Yes' then monthlyincome end ) - avg(case when overtime='No' then monthlyincome end )) as salary_difference
--- from retentiondata;
+-- Identify which department has higher attrition and how job satisfaction differs across departments
 
--- select (sum(case when attrition='Yes' then 1 else 0 end)*100/count(*)) as attrition_rate,MaritalStatus
--- from retentiondata
--- group by MaritalStatus; 
+select department,(sum(case when Attrition='Yes' then 1 else 0 end)*100.0/count(*)) as Attrition,avg(JobSatisfaction) as AVG_JOB_SATISFACTION from retentiondata
+group by department
+order by Attrition desc,
+AVG_JOB_SATISFACTION ASC
 
--- Find department with highest average performance rating.
+-- Check whether employees who work overtime earn more or less than those who do not
+  
+select (avg(case when overtime='Yes' then monthlyincome end ) - avg(case when overtime='No' then monthlyincome end )) as salary_difference
+from retentiondata;
 
--- select department,avg(performancerating) as AVG_Performance_rating
--- from retentiondata r
--- group by department
--- order by AVG_Performance_rating desc
--- limit 1;
+-- Analyze whether marital status has an impact on attrition
+
+select (sum(case when attrition='Yes' then 1 else 0 end)*100/count(*)) as attrition_rate,MaritalStatus
+from retentiondata
+group by MaritalStatus; 
+
+-- Identify the department with the best average performance rating
+
+select department,avg(performancerating) as AVG_Performance_rating
+from retentiondata 
+group by department
+order by AVG_Performance_rating desc
+limit 1;
