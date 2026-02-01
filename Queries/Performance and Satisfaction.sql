6a3b8fff77ebde60d576782f3d2a93e778fccc42
Hamza Nathwala <hamza.parvezali@gmail.com>
2026-02-01 09:55:12 +0530

    Update Performance and Satisfaction.sql

--- Queries/Performance and Satisfaction.sql
+++ Queries/Performance and Satisfaction.sql
@@ -1,27 +1,39 @@
--- select round(avg(JobSatisfaction),1) as AVG_Job_Satisfaction,department
--- from retentiondata
--- group by department;
+-- Compare average job satisfaction across departments
 
--- select employeenumber from retentiondata
--- where performancerating>=4;
+select round(avg(JobSatisfaction),1) as AVG_Job_Satisfaction,department
+from retentiondata
+group by department;
 
--- select employeenumber from retentiondata
--- where PerformanceRating>=4 and JobSatisfaction<=2
+-- Identify employees with high performance ratings (4 or above)
 
--- select round(avg(PerformanceRating),1) as AVG_PERFORMANCE , department
--- from retentiondata
--- group by department;
+select employeenumber from retentiondata
+where performancerating>=4;
 
--- select department,avg(JobSatisfaction) as AVG_JOB from retentiondata 
--- where (select avg(JobSatisfaction)from retentiondata ) < 2.5
--- group by department;
+-- Identify high-performing employees who have low job satisfaction
 
--- SELECT 
---     Department,
---     ROUND(AVG(jobsatisfaction), 2) AS AvgSatisfaction
--- FROM retentiondata
--- GROUP BY Department
--- HAVING AVG(JobSatisfaction) < 2.5;
+select employeenumber from retentiondata
+where PerformanceRating>=4 and JobSatisfaction<=2
+
+-- Compare average performance ratings across departments
+
+select round(avg(PerformanceRating),1) as AVG_PERFORMANCE , department
+from retentiondata
+group by department;
+
+-- Identify departments where average job satisfaction is below the overall company average
+
+select department,avg(JobSatisfaction) as AVG_JOB from retentiondata 
+where (select avg(JobSatisfaction)from retentiondata ) < 2.5
+group by department;
+
+-- Identify departments with consistently low job satisfaction
+
+SELECT 
+    Department,
+    ROUND(AVG(jobsatisfaction), 2) AS AvgSatisfaction
+FROM retentiondata
+GROUP BY Department
+HAVING AVG(JobSatisfaction) < 2.5;
 
 
 
