1b071b7fdb1a45a54bd1ea08359724ee7bafa790
Hamza Nathwala <hamza.parvezali@gmail.com>
2026-02-01 09:52:30 +0530

    Update Basic Employee Statistics.sql

--- Queries/Basic Employee Statistics.sql
+++ Queries/Basic Employee Statistics.sql
@@ -1,20 +1,28 @@
-use project;
+-- Find the total number of employees in the organization
 
--- SELECT COUNT(*) AS Total_Employees
--- FROM retentiondata;
+SELECT COUNT(*) AS Total_Employees
+FROM retentiondata;
 
--- SELECT Gender, COUNT(*) AS Total_Count
--- FROM retentiondata
--- GROUP BY Gender;
+-- Understand gender distribution across the organization
 
--- SELECT Department, COUNT(*) AS Total_Employees
--- FROM retentiondata
--- GROUP BY Department;
+SELECT Gender, COUNT(*) AS Total_Count
+FROM retentiondata
+GROUP BY Gender;
 
--- SELECT AVG(Age) AS Average_Age
--- FROM retentiondata;
+-- Identify department-wise employee distribution
 
--- SELECT EducationField, COUNT(*) AS Total_Employees
--- FROM retentiondata
--- GROUP BY EducationField;
+SELECT Department, COUNT(*) AS Total_Employees
+FROM retentiondata
+GROUP BY Department;
+
+-- Calculate the average age of employees in the organization
+
+SELECT AVG(Age) AS Average_Age
+FROM retentiondata;
+
+-- Analyze educational background distribution of employees
+
+SELECT EducationField, COUNT(*) AS Total_Employees
+FROM retentiondata
+GROUP BY EducationField;
 
