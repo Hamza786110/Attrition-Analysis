f0388e3f85ba806dced54b30bd8dc7b99f705345
Hamza Nathwala <hamza.parvezali@gmail.com>
2026-02-02 11:12:20 +0530

    Update Retention & Promotion Insights.sql

--- Queries/Retention & Promotion Insights.sql
+++ Queries/Retention & Promotion Insights.sql
@@ -1,24 +1,33 @@
-use project;
--- select employeenumber,monthlyincome 
--- from retentiondata
--- where TotalWorkingYears>5 and YearsSinceLastPromotion>=3;
+-- Identify employees with more than 5 years of experience who have not been promoted in the last 3 years
 
--- elect employeenumber,monthlyincome,department
--- from retentiondata e
--- where totalworkingyears>10 and monthlyincome<(select avg(monthlyincome) as AVG_DEPARTMENT_INCOME from retentiondata where department = e.department)
+select employeenumber,monthlyincome 
+from retentiondata
+where TotalWorkingYears>5 and YearsSinceLastPromotion>=3;
 
--- select employeenumber,department,performancerating
--- from (
--- select employeenumber,department,performancerating,
--- rank() over (partition by department order by performancerating desc) as rnk
--- from retentiondata) as ranked
--- where rnk=1;
+-- Identify employees with over 10 years of experience earning less than their department’s average income
 
--- select Department ,count(*) as Employeecount
--- from retentiondata
--- where TotalWorkingYears>10
--- group by department;
+select employeenumber,monthlyincome,department
+from retentiondata e
+where totalworkingyears>10 and monthlyincome<(select avg(monthlyincome) as AVG_DEPARTMENT_INCOME from retentiondata where department = e.department)
 
--- select employeenumber as Eligible_for_Promotion,Department,yearssincelastpromotion,performancerating
--- from retentiondata
--- where yearssincelastpromotion>3 and performancerating>=4;
+-- Identify the highest-performing employee in each department
+
+select employeenumber,department,performancerating
+from (
+select employeenumber,department,performancerating,
+rank() over (partition by department order by performancerating desc) as rnk
+from retentiondata) as ranked
+where rnk=1;
+
+-- Count employees with more than 10 years of experience in each department
+
+select Department ,count(*) as Employeecount
+from retentiondata
+where TotalWorkingYears>10
+group by department;
+
+-- Identify employees eligible for promotion based on performance and promotion gap
+
+select employeenumber as Eligible_for_Promotion,Department,yearssincelastpromotion,performancerating
+from retentiondata
+where yearssincelastpromotion>3 and performancerating>=4;
