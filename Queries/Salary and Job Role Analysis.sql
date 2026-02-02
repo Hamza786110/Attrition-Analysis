01f8cf840235f67278e531e1b6de1ef4f8d8da29
Hamza Nathwala <hamza.parvezali@gmail.com>
2026-02-02 11:14:29 +0530

    Update Salary and Job Role Analysis.sql

--- Queries/Salary and Job Role Analysis.sql
+++ Queries/Salary and Job Role Analysis.sql
@@ -1,25 +1,36 @@
--- select * from retentiondata;
--- select avg(MonthlyIncome) as AVG_MONTHLY_INCOME, JobRole 
--- from retentiondata
--- group by JobRole;
+-- Analyze average monthly income across different job roles
 
--- select round(avg(MonthlyIncome),1) as AVG_MONTHLY_INCOME, Department 
--- from retentiondata
--- group by Department;
+select avg(MonthlyIncome) as AVG_MONTHLY_INCOME, JobRole 
+from retentiondata
+group by JobRole;
 
--- select (Jobrole) as Job_Role,avg(MonthlyIncome) as AVG_INCOME
--- from retentiondata
--- group by Jobrole
--- order by AVG_INCOME desc limit 5;
+-- Compare average monthly income across departments
 
--- select EmployeeNumber,MonthlyIncome,Department
--- from retentiondata e
--- where MonthlyIncome>(select avg(MonthlyIncome) as Department_AVG_INCOME from retentiondata where department=e.department);
+select round(avg(MonthlyIncome),1) as AVG_MONTHLY_INCOME, Department 
+from retentiondata
+group by Department;
 
--- select avg(MonthlyIncome) as AVG_INCOME ,gender
--- from retentiondata
--- group by gender;`
+-- Identify the top 5 job roles with the highest average monthly income
 
--- select employeenumber,monthlyincome,department,YearsAtCompany
--- from retentiondata
--- where MonthlyIncome<(select avg(MonthlyIncome) as Company_AVG from retentiondata) and YearsAtCompany > 10;
+select (Jobrole) as Job_Role,avg(MonthlyIncome) as AVG_INCOME
+from retentiondata
+group by Jobrole
+order by AVG_INCOME desc limit 5;
+
+-- Identify employees earning more than their department's average income
+
+select EmployeeNumber,MonthlyIncome,Department
+from retentiondata e
+where MonthlyIncome>(select avg(MonthlyIncome) as Department_AVG_INCOME from retentiondata where department=e.department);
+
+-- Analyze average income differences based on gender
+
+select avg(MonthlyIncome) as AVG_INCOME ,gender
+from retentiondata
+group by gender;
+
+-- Identify employees with more than 10 years at the company earning below the overall company average income
+
+select employeenumber,monthlyincome,department,YearsAtCompany
+from retentiondata
+where MonthlyIncome<(select avg(MonthlyIncome) as Company_AVG from retentiondata) and YearsAtCompany > 10;
